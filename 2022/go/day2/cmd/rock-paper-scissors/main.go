338db1f7d7e013ed293ed7091e95b066ba28f765
Tom Scholtz <tom.scholtz@lifeq.com>
2022-12-04 10:26:09 +0200

    day2: go solution

---
+++ 2022/go/day2/cmd/rock-paper-scissors/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/tomschdev/advent-of-code/2022/go/day1/pkg/io"
+)
+
+var hardcodeResult = map[string]int{
+	"X": 0,
+	"Y": 3,
+	"Z": 6,
+}
+var chooseCorrectPlay = map[string]map[string]string{
+	"A": {
+		"X": "Z",
+		"Y": "X",
+		"Z": "Y",
+	},
+	"B": {
+		"X": "X",
+		"Y": "Y",
+		"Z": "Z",
+	},
+	"C": {
+		"X": "Y",
+		"Y": "Z",
+		"Z": "X",
+	},
+}
+var scorePerPlay = map[string]int{
+	"X": 1,
+	"Y": 2,
+	"Z": 3,
+}
+var referee = map[string]map[string]int{
+	"A": {
+		"X": 3,
+		"Y": 6,
+		"Z": 0,
+	},
+	"B": {
+		"X": 0,
+		"Y": 3,
+		"Z": 6,
+	},
+	"C": {
+		"X": 6,
+		"Y": 0,
+		"Z": 3,
+	},
+}
+
+func main() {
+	// question1()
+	question2()
+}
+
+func question1() {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	contents := io.ReadInputBytes(wd + "/input.txt")
+	rounds := strings.Split(strings.Trim(string(contents), " "+"\n"), "\n")
+	score := 0
+	for _, r := range rounds {
+		score += scoreRound(strings.Split(r, " "))
+	}
+	fmt.Println(score)
+}
+
+func question2() {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	contents := io.ReadInputBytes(wd + "/input.txt")
+	rounds := strings.Split(strings.Trim(string(contents), " "+"\n"), "\n")
+	score := 0
+	for _, r := range rounds {
+		score += hackRound(strings.Split(r, " "))
+	}
+	fmt.Println(score)
+}
+
+func scoreRound(plays []string) int {
+	if len(plays) != 2 {
+		fmt.Println("expected 2 plays, got: ", plays)
+		os.Exit(1)
+	}
+	return referee[plays[0]][plays[1]] + scorePerPlay[plays[1]]
+}
+
+func hackRound(plays []string) int {
+	if len(plays) != 2 {
+		fmt.Println("expected 2 plays, got: ", plays)
+		os.Exit(1)
+	}
+	return hardcodeResult[plays[1]] + scorePerPlay[chooseCorrectPlay[plays[0]][plays[1]]]
+}
---
+++ 2022/go/day2/go.mod
@@ -0,0 +1,5 @@
+module github.com/tomschdev/advent-of-code/2022/go/day2
+
+go 1.19
+
+require github.com/tomschdev/advent-of-code/2022/go/day1 v0.0.0-20221201195752-b1f49c477dc8 // indirect
